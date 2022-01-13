de63d9dfdc22baf2a377ad98bfc6c680557f5f4a
rjohnsonanr <robjohnson@ucanr.edu>
2022-01-13 10:52:44 -0800

    Profile

--- waterfowl_tracker/tracker/models.py
+++ waterfowl_tracker/tracker/models.py
@@ -1,8 +1,5 @@
 from django.contrib.gis.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 
 intType = [
     ('Always', 'Always'),
@@ -10,19 +7,19 @@ intType = [
 ]
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to="tracker/profiles/avatars/", null=True, blank=True)
+    company = models.CharField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=32, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    @property
+    def get_avatar(self):
+        return self.avatar.url if self.avatar else 'https://github.com/mdo.png'
 
 class FarmLoc(models.Model):
     name = models.CharField('Farm Name', max_length=100)
--- waterfowl_tracker/tracker/urls.py
+++ waterfowl_tracker/tracker/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
